4d0d0fb42e8fa0bbd20f094ceedc66019afbe849
Luis Guilherme da Cruz <guicruz95@hotmail.com>
2025-10-14 13:37:07 -0300

    Implementei a borda preta de todo projeto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// App.jsx
+import './App.css';
+import './styles/global.css';
+import Home from '../src/components/Home';
+
+
+function App() {
+  return (
+    <div className="app_container">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
